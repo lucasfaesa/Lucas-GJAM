fc192f8d71e5e67c78afe22c4a5a400a6ab5d9ee
Lucas Rios <lucasfaesa@gmail.com>
2020-07-20 23:21:58 -0300

    - HUD dos itens selecionads

---
+++ Assets/Scripts/RotateObjects.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObjects : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * (speed * Time.deltaTime),Space.World);
+    }
+}
